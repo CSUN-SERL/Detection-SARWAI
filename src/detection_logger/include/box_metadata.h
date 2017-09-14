0ddbf431beffec8ababf036fab7369e02bff8d4e
Logan G <logangerber1014@yahoo.com>
2017-09-14 01:34:12 -0700

    Added TODO to box metadata struct. We'll get this info from the ros subscriber when we add it in.

--- src/detection_logger/include/box_metadata.h
+++ src/detection_logger/include/box_metadata.h
@@ -1,6 +1,9 @@
 #ifndef SARWAI_DETECTION-LOGGER_BOX-METADATA_H_
 #define SARWAI_DETECTION-LOGGER_BOX-METADATA_H_
 
+// TODO: Delete this whole file when integrating with the Matt and Aren's part.
+// This will all be contained in the message we get from the rostopic. Silly me
+
 namespace sarwai{
   struct BoxMetadata{
     double confidence_rating;
