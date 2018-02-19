25ddf027de0d750e76e4950ab9a695aa7182d5f3
ArenBoghozian8 <aren.mboghozian@gmail.com>
2018-02-19 12:43:59 -0800

    can specify what branch to push to stations

--- update-stations.sh
+++ update-stations.sh
@@ -1,10 +1,16 @@
+# Can specifiy a particular branch to propagate to machines as the first argument.
+# Defaults to master if no argument is supplied
+if 
+  [ -z ${1+master} ];
+fi
+
 for i in serl@station1 serl@station3 serl@station4 station@station5 serl@azeroth
 do
     echo $i
     ssh $i << 'ENDSSH'
 cd ~/Documents/detection
 git checkout master
-git pull origin master
+git pull origin $1
 source devel/setup.bash
 catkin_make
 ENDSSH
